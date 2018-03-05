e8baddf75733b2f3772a9b869f49b9f932dfaa7c
SirFurness <eric.1334@icloud.com>
2018-03-04 18:30:56 -0800

    Use state monad and space subsets

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,38 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Lib where
+import Control.Monad.State
 import Data.Set
 
 data Point = Point [Char]
                deriving (Show, Eq, Ord)
-universal = fromList [[Point "a", Point "b", Point "c", Point "d"], []]
+-- use for input to state:(fromList [Point "a", Point "b", Point "c", Point "d"], [])
+data SpaceSubset = forall s. (SubsetOfSpace s, Show s) => SpaceSubset s
+type SpaceWithSubsets = (Set Point, [SpaceSubset])
+type Geometry a = State SpaceWithSubsets a
+
 data Line = Line (Set Point)
+  deriving (Show, Eq)
 
-class SpaceSubset a where
+class SubsetOfSpace a where
   getSet :: a -> Set Point
+instance Show SpaceSubset where
+  show (SpaceSubset a) = show a
+instance SubsetOfSpace SpaceSubset where
+  getSet (SpaceSubset a) = getSet a
 
-instance SpaceSubset Line where
+instance SubsetOfSpace Line where
   getSet (Line set) = set
 
-isCollinear :: [Point] -> Bool
+getSubsets :: Geometry [SpaceSubset]
+getSubsets = do
+  universe <- get
+  return $ snd universe
+
+getPoints :: Geometry (Set Point)
+getPoints = do
+  universe <- get
+  return $ fst universe
+
+--isCollinear :: [Point] -> Geometry Bool
+--isCollinear points = do
+  
