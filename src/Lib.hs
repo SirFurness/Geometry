af0bb7843afa2f108876fbc68bdc1cecabf8f37d
SirFurness <eric.1334@icloud.com>
2018-03-04 20:45:10 -0800

    Remake all of SpaceSubset

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,21 @@
-{-# LANGUAGE ExistentialQuantification #-}
 module Lib where
 import Control.Monad.State
-import Data.Set
+import Data.Set as S
+import Prelude as P
 
 data Point = Point [Char]
-               deriving (Show, Eq, Ord)
--- use for input to state:(fromList [Point "a", Point "b", Point "c", Point "d"], [])
-data SpaceSubset = forall s. (SubsetOfSpace s, Show s) => SpaceSubset s
-type SpaceWithSubsets = (Set Point, [SpaceSubset])
-type Geometry a = State SpaceWithSubsets a
+               deriving (Show, Eq, Ord) --Ord is necessary of use in Set
 
-data Line = Line (Set Point)
-  deriving (Show, Eq)
+data SubsetType = Line
+            deriving (Show, Eq)
 
-class SubsetOfSpace a where
-  getSet :: a -> Set Point
-instance Show SpaceSubset where
-  show (SpaceSubset a) = show a
-instance SubsetOfSpace SpaceSubset where
-  getSet (SpaceSubset a) = getSet a
+data SpaceSubset = SpaceSubset { subsetType :: SubsetType,
+                                 set :: Set Point,
+                                 name :: [Char]
+                               }
+                     deriving (Show)
 
-instance SubsetOfSpace Line where
-  getSet (Line set) = set
+type Geometry a = State (Set Point, [SpaceSubset]) a
 
 getSubsets :: Geometry [SpaceSubset]
 getSubsets = do
@@ -33,6 +27,28 @@ getPoints = do
   universe <- get
   return $ fst universe
 
---isCollinear :: [Point] -> Geometry Bool
---isCollinear points = do
-  
+makeLine :: [Point] -> [Char] -> Geometry ()
+makeLine givenPoints name = do
+  points <- getPoints
+  subsets <- getSubsets
+  let newPoints = union points $ fromList givenPoints
+  let line = SpaceSubset Line (fromList givenPoints) name
+  let newSubsets = line : subsets
+  put (newPoints, newSubsets)
+
+isLine :: SpaceSubset -> Bool
+isLine subset
+  | subsetType subset == Line = True
+  | otherwise = False
+
+getLines :: Geometry [SpaceSubset]
+getLines = do
+  subsets <- getSubsets
+  return $ P.filter isLine subsets
+
+isCollinear :: Set Point -> Geometry Bool
+isCollinear points
+  | size points < 3 = return True --Note this returns true for an empty set
+  | otherwise = do
+      lines <- getLines
+      return $ any (points `isSubsetOf`) $ P.map set lines
